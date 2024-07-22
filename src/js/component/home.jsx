f9588e36eef3f83dbd86bbe721ea401e72c895fa
4lvaro8 <ponitsa@gmail.com>
2024-07-22 18:37:21 +0000

    sin fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+    const [toDos, settoDos] = useState([]);
+    const [toDoInput, settoDoInput] = useState("");
+
+
+    function ToDoItem({ label, delete_toDo }) {
+        return (
+            <div className="toDo-item">
+                <span className='toDo-text'>{label}</span>
+                <div className='deletetrash' onClick={delete_toDo}><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+                </svg>
+                </div>
+            </div>
+        );
+    }
+    
+
+    return (
+        <div className='main container-fluid w-50'>
+
+            <form className='form container-fluid d-flex flex-column ' onSubmit={(e) => {
+                e.preventDefault();
+
+                if (toDoInput.length > 0) {
+                    settoDos([{
+                        label: toDoInput,
+                        is_done: false
+                    }, ...toDos])
+                    settoDoInput("");
+                }
+            }}>
+
+                <h1 className="title align-self-center">To Do List</h1>
+
+                <div className="tareas">
+                    <input className="form-control"
+                        type="text"
+                        placeholder="Que debes hacer?"
+                        aria-label="todo list input field"
+                        value={toDoInput}
+                        onChange={(e) => settoDoInput(e.target.value)}
+                    />
+
+                    {toDos.map((item, id) =>
+                        <ToDoItem key={id} label={item.label}
+                            delete_toDo={() => settoDos(toDos.toSpliced(id, 1))}
+                        />)}
+
+                    <p><small>{toDos.length} Que haceres restantes{toDos.length == 0 ? ", agregue una tarea" : ""}</small></p>
+                </div>
+
+
+            </form>
+        </div>
+
+    )
+}
 
 export default Home;
